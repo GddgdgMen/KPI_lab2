57ed7ffade1c0bc6bca58541a157cbb778bad3ef
Danylo Holets <goletsdan@gmail.com>
2023-03-20 20:11:33 +0200

    Update main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,26 +3,70 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 
 	lab2 "github.com/GddgdgMen/KPI_lab2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inFile          = flag.String("f", "", "Input file name")
+	outFile         = flag.String("o", "", "Output file name")
 )
 
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if *inputExpression == "" && *inFile == "" {
+		log.Fatal("no expression provided. use -e \"{expression}\" or -f {file with expression}")
+	}
 
-	res, _ := lab2.PostfixToInfix("+ 2 2")
-	fmt.Println(res)
+	var reader io.Reader
+
+	if *inputExpression != "" {
+		reader = strings.NewReader(*inputExpression)
+	} else {
+		file, err := os.Open(*inFile)
+		if err != nil {
+			log.Fatal("no such file")
+		}
+		reader = file
+		defer file.Close()
+	}
+
+	var writer io.Writer
+
+	if *outFile != "" {
+		file, err := os.Create(*outFile)
+		if err != nil {
+			log.Fatal("something went wrong while creating file")
+		}
+
+		writer = file
+		defer file.Close()
+	} else {
+		writer = &Writer{}
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  reader,
+		Output: writer,
+	}
+
+	err := handler.Compute()
+
+	if err != nil {
+		fmt.Print(lab2.ThrowError())
+	}
+
+}
+
+type Writer struct{}
+
+func (w *Writer) Write(data []byte) (n int, err error) {
+	fmt.Println(string(data))
+	return len(data), nil
 }
